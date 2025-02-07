a7b1a3e6cb48bad6cf711deafc5be7add473af46
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-02-07 06:22:31 -0300

    ajuste gauge controller apos remover service

--- observabilitypoc/src/main/java/com/observabilitypoc/gague/GaugeController.java
+++ observabilitypoc/src/main/java/com/observabilitypoc/gague/GaugeController.java
@@ -1,28 +1,35 @@
 package com.observabilitypoc.gague;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/pedidos")
 public class GaugeController {
-    private final GaugeService gaugeService;
 
-    public GaugeController(GaugeService metricService) {
-        this.gaugeService = metricService;
+    private final AtomicInteger pedidosEmProcessamento = new AtomicInteger(0);
+
+    public GaugeController(MeterRegistry meterRegistry) {
+        Gauge.builder("pedidos_em_processamento", pedidosEmProcessamento, AtomicInteger::get)
+                .description("Número de pedidos em processamento")
+                .register(meterRegistry);
     }
 
     @PostMapping("/incrementar")
     public ResponseEntity<String> incrementarPedido() {
-        int gaugeAtual = gaugeService.incrementarPedido();
+        int gaugeAtual = pedidosEmProcessamento.incrementAndGet();
         return ResponseEntity.ok(String.format("Pedido adicionado! GaugeAtual: %d", gaugeAtual));
     }
 
     @PostMapping("/decrementar")
     public ResponseEntity<String> decrementarPedido() {
-        int gaugeAtual = gaugeService.decrementarPedido();
+        int gaugeAtual = pedidosEmProcessamento.decrementAndGet();
         return ResponseEntity.ok(String.format("Pedido adicionado! GaugeAtual: %d", gaugeAtual));
     }
 
