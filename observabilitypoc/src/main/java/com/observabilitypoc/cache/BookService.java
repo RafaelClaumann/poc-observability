02aa408ad265f484e3c14fc66a4bd8e9be079c33
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-03-14 08:17:17 -0300

    adicionando condition ao cache

--- observabilitypoc/src/main/java/com/observabilitypoc/cache/BookService.java
+++ observabilitypoc/src/main/java/com/observabilitypoc/cache/BookService.java
@@ -16,35 +16,50 @@ public class BookService {
 
     private static final Logger log = LoggerFactory.getLogger(BookService.class);
 
+    // is public static to be accessible on @Cacheable annotation of getBookByISBN method
+    public static final String BOOK_ISBN_WILL_NOT_BE_CACHED = "9788550804606";
+    private static final String BOOK_ISBN_WILL_SLEEP_FIRST_CALL = "9788575223920";
+
+
     private final List<Book> javaBooks = new ArrayList<>(
             List.of(
                     new Book("Java: Como Programar", "Deitel", "9788576055643"),
                     new Book("Use a Cabeça! Java", "Kathy Sierra, Bert Bates", "9788576081734"),
-                    new Book("Java Efetivo", "Joshua Bloch", "9788550804606"),
-                    new Book("Padrões de Projetos com Java", "Eduardo Guerra", "9788575223920")
+                    new Book("Java Efetivo", "Joshua Bloch", BOOK_ISBN_WILL_NOT_BE_CACHED),
+                    new Book("Padrões de Projetos com Java", "Eduardo Guerra", BOOK_ISBN_WILL_SLEEP_FIRST_CALL)
             )
     );
 
-    @Cacheable(cacheNames = "books", key = "#isbn")
+
+    @CacheEvict(cacheNames = "books", key = "#isbn")
+    public boolean deleteBookByISBN(final String isbn) {
+        log.info("Trying to delete book by ISBN: {}", isbn);
+        return javaBooks.removeIf(book -> book.isbn().equalsIgnoreCase(isbn));
+    }
+
+    // another way to access class attribute is: T(<package>.<class_name>).<class_static_attribute>
+    // will cache if and only if isbn received in getBookByISBN is not equals to BOOK_ISBN_WILL_NOT_BE_CACHED
+    @Cacheable(cacheNames = "books", key = "#isbn", condition = "!#isbn.equalsIgnoreCase(@bookService.BOOK_ISBN_WILL_NOT_BE_CACHED)")
     public Book getBookByISBN(final String isbn) {
+
+        // will sleep in EVERY call for ISBN #BOOK_ISBN_WILL_NOT_BE_CACHED
+        // will sleep in FIRST call for ISBN #BOOK_ISBN_WILL_SLEEP_FIRST_CALL
+        if (isbn.equalsIgnoreCase(BOOK_ISBN_WILL_NOT_BE_CACHED) || isbn.equalsIgnoreCase(BOOK_ISBN_WILL_SLEEP_FIRST_CALL)) {
+            try {
+                log.info("Sleeping when get book by ISBN = {}", isbn);
+                Thread.sleep(5000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
         final Book result = javaBooks.stream()
                 .filter(book -> book.isbn().equalsIgnoreCase(isbn))
                 .findFirst()
                 .orElseThrow(() -> new BookNotFoundException("Book not found with ISBN: " + isbn));
 
-        log.info("Book found: {}", result);
+        log.info("Book found with ISBN: {}", result);
         return result;
-    }
-
-    @CacheEvict(cacheNames = "books", key = "#isbn")
-    public boolean deleteBookByISBN(final String isbn) {
-        if (javaBooks.removeIf(book -> book.isbn().equalsIgnoreCase(isbn))) {
-            log.info("Book deleted: {}", isbn);
-            return true;
-        }
-
-        log.info("Book not found: {}", isbn);
-        return false;
     }
 
 }
