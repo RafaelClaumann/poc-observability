68dfb547b26138f5adc536b0343279d040e4980b
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-03-14 09:11:01 -0300

    adicionando redis e config da serializacao
    essa configuracao se serializacao foi necessaria porque o Record nao implementa Serializable

---
+++ observabilitypoc/src/main/java/com/observabilitypoc/cache/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.observabilitypoc.cache;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        final RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofMinutes(5));
+
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(cacheConfig).build();
+    }
+
+}
